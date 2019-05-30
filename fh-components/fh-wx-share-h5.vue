6363a4dd271c163a7b07244b51ce2e11df4d09d1
feihua <feihua666@sina.com>
2019-05-30 15:13:57 +0800

    又改一版本样式

--- fh-components/fh-wx-share-h5.vue
+++ fh-components/fh-wx-share-h5.vue
@@ -31,8 +31,8 @@
                 if(!shareContent.link){
                     link = window.location.href
                 }
-                let singlemessageLink = self.$utils.concatUrlHashParam(link,{from: 'singlemessage',f_u: self.$store.state.userinfo.id})
-                let timelineLink = self.$utils.concatUrlHashParam(link,{from: 'timeline',f_u: self.$store.state.userinfo.id})
+                let singlemessageLink = self.$utils.concatUrlParam(link,{from: 'singlemessage',f_u: self.$store.state.userinfo.id})
+                let timelineLink = self.$utils.concatUrlParam(link,{from: 'timeline',f_u: self.$store.state.userinfo.id})
                 jWeixin.error(function(res){
                     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
                 });
